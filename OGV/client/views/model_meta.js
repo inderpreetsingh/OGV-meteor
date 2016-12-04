55a4036bc320172157195a3b7cacbb1e05b41a5c
Phacometer <victorgao44@gmail.com>
2016-12-04 10:53:22 -0400

    Fix spelling errors

--- OGV/client/views/model_meta.js
+++ OGV/client/views/model_meta.js
@@ -1,4 +1,4 @@
-/*                     M O D E L _ M E T A . J S
+/**                     M O D E L _ M E T A . J S
  * BRL-CAD
  *
  * Copyright (c) 1995-2013 United States Government as represented by
@@ -36,32 +36,92 @@ Template.modelMeta.events({
     'submit #uploader-form': function(e, t)
     {
 	e.preventDefault();
-	
+	 a = e;
 	var modelMetaForm = $(e.currentTarget),
 	    filename = modelMetaForm.find('#desc-filename').val().toLowerCase(),
 	    description = modelMetaForm.find('#desc-about').val(),
-	    thumbnail,
-	    modelId = modelMetaForm.find('#model-id').val();
+	    modelId = modelMetaForm.find('#model-id').val(),
+	    audience = modelMetaForm.find('#desc-audience').val(),
+	    currentUser = Meteor.user();
 	    
-	file = $('#desc-model-thumb')
-	
-	var fsFile = new FS.File(e.target[2].files[0]);
-	fsFile.gFile = modelId;
-        	
-	ThumbFiles.insert(fsFile,function(err,thumbFile) {
-	    if (err) {
-		throwError(err.reason);
+	/**
+	* Adding the checked boxes to an array named category
+	*/
+	var category = Array();
+	$("input:checkbox[name=category]:checked").each(function(){
+    	category.push($(this).val());
+	});
+
+	var currentModel = ModelFiles.findOne(modelId);		
+
+	ModelFiles.update(modelId, {$set: {name: filename, about: description, audience: audience}}, function(error, res) {
+	    if (error) {
+			sAlert.error(error.reason);
 	    } else {
-		throwNotification("Image has been Uploaded" );
-		ModelFiles.update(modelId, {$set: {name: filename, about: description, thumbnail:thumbFile._id}}, function(error, res) {
-		    if (error) {
-			throwError(error.reason);
-		    } else {
-			throwNotification("Data about model has been saved");
-		    }
-		});
-  
+			sAlert.success("Data about model has been saved");
 	    }
-	}); 
-    } 
+	});
+	Posts.insert({
+		postType: "posted",
+		postedAt: currentModel.timeUploaded,
+		postId: modelId,
+		postedBy: currentUser._id,
+		audience: audience
+	});
+	if(category.length > 0){
+		ModelFiles.update(modelId, {$set: {categories: category}}, function(error, res) {
+	    if (error) {
+	    	sAlert.error(error.reason);
+	    } else {
+			sAlert.success("Data about model has been saved");					
+	    }
+	});
+	}
+	/*cPercent = Meteor.call('convertPercent');
+	console.log("######");
+	console.log(cPercent);
+	console.log("######");*/
+
+	var uploadedModel = ModelFiles.findOne(modelId);
+	if( uploadedModel.converted ){
+		Router.go('/models/'+uploadedModel._id);
+		sAlert.success("Data about model has been saved");
+	} else {
+		ModelFiles.remove(uploadedModel._id);
+		Router.go('/upload');
+		sAlert.success("There was some error in converting your uploaded file");
+	}
+		 
+
+		    
+}
+});		
+
+Template.modelMeta.helpers({
+    'progressValue': function(){
+	        var id = $('#model-id').val(),
+		modelObj = ModelFiles.findOne({_id: id});
+		var value = modelObj.conversion;
+		value = parseInt(value, 10);
+		if(value == null) value = 0;
+		if(value >= 70) {
+			$('progress[value]').css('display', 'none');
+			$('.progress-label').css('display', 'none');
+			$('#save-btn').css('display', 'block');
+			return value;
+		} else {
+		return value;
+		}
+	}
 });
+
+/**
+* helper to display already present categories in the model
+* Displayed every time when the /description/:_id page is viewed
+* Displays nothing if categories is empty.
+*/
+Template.modelMeta.modelCategory = function() 
+{
+    var id = Session.get('modelId');
+    return ModelFiles.findOne({_id: id}); 
+};

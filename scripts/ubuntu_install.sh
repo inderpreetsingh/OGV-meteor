f68bbd93ecf11f9cb8a56882438507e6f054cc21
ih8sum3r <ih8sum3r@gmail.com>
2018-03-10 01:09:26 -0500

    removed unexpected warning after update

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -8,7 +8,7 @@ echo " "
 
 # Node
 echo "# Installing node"
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 echo "# Node version"

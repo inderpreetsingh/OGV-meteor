96659cfdc617874e70d052c6c6a17c20ed398c46
Daniel Rossberg <33295041+drossberg@users.noreply.github.com>
2021-07-18 19:51:44 +0200

    close drop down menus automatically when another one is open
    Fixes issue #61
    Applies pull request #95

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -3,9 +3,11 @@ import "./header.scss";
 
 Template.header.events({
   "click #header-profile-button": function() {
+    $("#header-notification-menu").addClass("menu-hidden");
     $("#header-profile-menu").toggleClass("menu-hidden");
   },
   "click #header-notification-button": function() {
+    $("#header-profile-menu").addClass("menu-hidden");
     $("#header-notification-menu").toggleClass("menu-hidden");
   },
   "click #log-out": function(e, t) {
